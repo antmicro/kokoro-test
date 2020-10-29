59b46a2dc020db2fcb38d0a28a242fb9bd74d692
Adam Olech <aolech@antmicro.com>
2020-10-29 17:41:39 +0100

    Try to fetch invocation

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ls -alh
+
 cd github/$KOKORO_DIR/
 
 source ./steps/hostsetup.sh
@@ -12,3 +14,7 @@ export GIT_CHECKOUT=$PWD
 export GIT_DESCRIBE=$(git describe --match v*)
 
 echo "$KOKORO_TYPE run"
+
+wget https://raw.githubusercontent.com/antmicro/distant-rs/master/examples/get_inv.py
+
+python3 get_inv.py a69ce84c-54dd-46a5-a2d3-3ec29ea445e2
