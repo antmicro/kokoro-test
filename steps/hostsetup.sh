755596c3b5be123bd51e5bc22a32306e701a14c2
Adam Olech <aolech@antmicro.com>
2020-10-29 17:01:23 +0100

    Try to install newer python

--- steps/hostsetup.sh
+++ steps/hostsetup.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sudo add-apt-repository ppa:deadsnakes/ppa
+
 echo
 echo "========================================"
 echo "Host updating packages"
@@ -21,10 +23,17 @@ sudo apt-get install -y \
         curl \
         git \
         wget \
-	python3 \
-	python3-pip \
+	python3.8 \
+	python3.8-dev \
+	python3.8-venv \
 	screenfetch
 
 screenfetch
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+sudo update-alternatives --config python3
+
+curl https://bootstrap.pypa.io/get-pip.py | sudo python3.8
 
 sudo pip3 install git+https://github.com/antmicro/distant-rs.git
