825ab5cd5b2168edf527d400bcaaff937695b9d0
Adam Olech <aolech@antmicro.com>
2020-10-29 15:15:59 +0100

    Install python3 and output os-release

--- steps/hostsetup.sh
+++ steps/hostsetup.sh
@@ -4,52 +4,24 @@ set -e
 
 echo
 echo "========================================"
-echo "Removing older packages"
-echo "----------------------------------------"
-sudo apt-get remove -y cmake
-echo "----------------------------------------"
-
-echo
-echo "========================================"
-echo "Host adding PPAs"
-echo "----------------------------------------"
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ xenial main'
-echo "----------------------------------------"
-
-echo
-echo "========================================"
 echo "Host updating packages"
 echo "----------------------------------------"
-sudo apt-get update
+sudo apt -qqy update && sudo apt -qqy upgrade
 echo "----------------------------------------"
 
-echo
-echo "========================================"
-echo "Host remove packages"
-echo "----------------------------------------"
-sudo apt-get remove -y \
-	python-pytest \
-
-
-sudo apt-get autoremove -y
-
-echo "----------------------------------------"
 echo
 echo "========================================"
 echo "Host install packages"
 echo "----------------------------------------"
 sudo apt-get install -y \
         bash \
-        bison \
         build-essential \
         ca-certificates \
-        clang-format \
-        cgroup-tools \
-        cmake \
-        colordiff \
         coreutils \
         curl \
-        flex \
         git \
         wget \
+	python3 \
+	python3-pip
+
+sudo pip3 install git+https://github.com/antmicro/distant-rs.git
