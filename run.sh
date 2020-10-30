aeea02a99965bb32afec3f20279c79e3c18af9cc
Adam Olech <aolech@antmicro.com>
2020-10-30 18:34:17 +0100

    Try to set the project via an env var

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ source ./steps/git.sh
 
 export GIT_CHECKOUT=$PWD
 export GIT_DESCRIBE=$(git describe --match v*)
+export GOOGLE_CLOUD_PROJECT=foss-fpga-tools-ext-antmicro
 
 echo "$KOKORO_TYPE run"
 
