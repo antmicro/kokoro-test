f6cb5bd3d2914f6eead44d170db05a1a588343c8
Adam Olech <aolech@antmicro.com>
2020-11-27 15:07:49 +0000

    Get proper credentials

--- run.sh
+++ run.sh
@@ -14,10 +14,14 @@ export GIT_CHECKOUT=$PWD
 export GIT_DESCRIBE=$(git describe --match v*)
 export GOOGLE_CLOUD_PROJECT=foss-fpga-tools-ext-antmicro
 export GCLOUD_PROJECT=foss-fpga-tools-ext-antmicro
+export GOOGLE_APPLICATION_CREDENTIALS=$KOKORO_KEYSTORE_DIR/tflite.json
+export GOOGLE_PROJECT_ID=292154118698
+export DISTANT_RS_BUCKET=tflite-tests-artifacts
 
 echo "$KOKORO_TYPE run"
 
 cp example.xml sponge_log.xml
+gsutil cp gs://tflite-tests-priv-bucket/secrets/tflite.json $GOOGLE_APPLICATION_CREDENTIALS
 
 # run Renode
 
@@ -25,3 +29,5 @@ renode --disable-xwt -e "q"
 find  /opt/renode/tests/platforms -name *robot -exec echo - {} >> ci_tests.yaml \; -quit
 
 python3 test_and_publish.py -t ci_tests.yaml -j`nproc` -P 12000
+
+rm $GOOGLE_APPLICATION_CREDENTIALS
