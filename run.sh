32bd40dd3a5c42dc3d50e33684293bc570254f69
Adam Olech <aolech@antmicro.com>
2020-11-30 17:02:33 +0000

    Remove example.xml

--- run.sh
+++ run.sh
@@ -12,15 +12,12 @@ source ./steps/git.sh
 
 export GIT_CHECKOUT=$PWD
 export GIT_DESCRIBE=$(git describe --match v*)
-export GOOGLE_CLOUD_PROJECT=foss-fpga-tools-ext-antmicro
-export GCLOUD_PROJECT=foss-fpga-tools-ext-antmicro
 export GOOGLE_APPLICATION_CREDENTIALS=$KOKORO_KEYSTORE_DIR/tflite.json
 export GOOGLE_PROJECT_ID=292154118698
 export DISTANT_RS_BUCKET=tflite-tests-artifacts
 
 echo "$KOKORO_TYPE run"
 
-cp example.xml sponge_log.xml
 gsutil cp gs://tflite-tests-priv-bucket/secrets/tflite.json $GOOGLE_APPLICATION_CREDENTIALS
 
 # run Renode
