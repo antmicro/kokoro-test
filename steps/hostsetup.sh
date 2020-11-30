255f7e111b4afec54bb6587355cf040283fbcbc6
Adam Olech <aolech@antmicro.com>
2020-11-30 13:32:13 +0000

    Use stock google-auth library (installed by means of requirements)

--- steps/hostsetup.sh
+++ steps/hostsetup.sh
@@ -37,7 +37,6 @@ sudo update-alternatives --config python3
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3.8
 
-sudo -H pip3 install git+https://github.com/AdamOlech/google-auth-library-python.git
 sudo -H pip3 install git+https://github.com/antmicro/distant-rs.git
 
 curl -o gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-316.0.0-linux-x86_64.tar.gz
